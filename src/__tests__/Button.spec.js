f46be20c6cdd9cdaa3d05f4cbf5545a90b794a8b
charkala <charkala@protonmail.com>
2020-03-03 11:08:56 +0900

    Updated tests

---
+++ src/__tests__/Button.spec.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { render } from '@testing-library/react'
+
+function Button(props) {
+  return <button>Test Button</button>
+}
+
+describe('Button component', () => {
+  test('Matches the snapshot', () => {
+    const button = render(<Button />)
+    expect(button).toMatchSnapshot()
+  })
+})
