97ce06b0c27343d64fdd8ea345118144b5b259ca
charkala <charkala@protonmail.com>
2021-02-12 21:10:08 +0900

    Update imports

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import store from './redux/store';
 
-import './index.css';
+import './styles/global.css';
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
